551e6538624d301378a142decaf820f4ab2a4f3c
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-08-31 20:21:41 -0400

    Snap Piece to grid

--- Assets/Scripts/ChessPieces.cs
+++ Assets/Scripts/ChessPieces.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ChessPieces : MonoBehaviour
@@ -31,12 +32,17 @@ public class ChessPieces : MonoBehaviour
 
         Gizmos.color = Color.red;
 
+        
+        transform.position = SnapToGrid(transform.position);
         Vector3 pos = transform.position;
 
+        List<Vector3> moves = new List<Vector3>();
         switch (piece)
         {
             case chessPieces.pawn:
                 Gizmos.DrawLine(pos, pos + transform.up * 1f);
+                AddAllMovesInDirection(pos, moves, new Vector3(0, 1, 0), 1);
+
                 spriteRenderer.sprite = pawnSprite;
                 break;
 
@@ -64,10 +70,26 @@ public class ChessPieces : MonoBehaviour
 
                         Gizmos.DrawLineList(knightLines);
                 }
+                
+                AddAllMovesInDirection(pos, moves, new Vector3(2, -1, 0), 1);
+                AddAllMovesInDirection(pos, moves, new Vector3(2, 1, 0), 1);
+                AddAllMovesInDirection(pos, moves, new Vector3(-2, 1, 0), 1);
+                AddAllMovesInDirection(pos, moves, new Vector3(-2, -1, 0), 1);
+                AddAllMovesInDirection(pos, moves, new Vector3(-1, 2, 0), 1);
+                AddAllMovesInDirection(pos, moves, new Vector3(1, 2, 0), 1);
+                AddAllMovesInDirection(pos, moves, new Vector3(1, -2, 0), 1);
+                AddAllMovesInDirection(pos, moves, new Vector3(-1, -2, 0), 1);
                 break;
 
             case chessPieces.bishop:
                 spriteRenderer.sprite = bishopSprite;
+
+                AddAllMovesInDirection(pos, moves, new Vector3(1, 1, 0), 8);
+                AddAllMovesInDirection(pos, moves, new Vector3(-1, 1, 0), 8);
+                AddAllMovesInDirection(pos, moves, new Vector3(1, -1, 0), 8);
+                AddAllMovesInDirection(pos, moves, new Vector3(-1, -1, 0), 8);
+                
+                
                 Gizmos.DrawLine(pos, pos + (transform.up + transform.right).normalized * 4f);
                 Gizmos.DrawLine(pos, pos + (transform.up - transform.right).normalized * 4f);
                 Gizmos.DrawLine(pos, pos + (-transform.up + transform.right).normalized * 4f);
@@ -80,6 +102,11 @@ public class ChessPieces : MonoBehaviour
                 Gizmos.DrawLine(pos, pos - transform.up * 4f);
                 Gizmos.DrawLine(pos, pos + transform.right * 4f);
                 Gizmos.DrawLine(pos, pos - transform.right * 4f);
+                
+                AddAllMovesInDirection(pos, moves, new Vector3(-1, 0, 0), 8);
+                AddAllMovesInDirection(pos, moves, new Vector3(1, 0, 0), 8);
+                AddAllMovesInDirection(pos, moves, new Vector3(0, 1, 0), 8);
+                AddAllMovesInDirection(pos, moves, new Vector3(0, -1, 0), 8);
                 break;
 
             case chessPieces.queen:
@@ -92,6 +119,17 @@ public class ChessPieces : MonoBehaviour
                 Gizmos.DrawLine(pos, pos + (transform.up - transform.right).normalized * 4f);
                 Gizmos.DrawLine(pos, pos + (-transform.up + transform.right).normalized * 4f);
                 Gizmos.DrawLine(pos, pos + (-transform.up - transform.right).normalized * 4f);
+                
+                
+                AddAllMovesInDirection(pos, moves, new Vector3(1, 1, 0), 8);
+                AddAllMovesInDirection(pos, moves, new Vector3(-1, 1, 0), 8);
+                AddAllMovesInDirection(pos, moves, new Vector3(1, -1, 0), 8);
+                AddAllMovesInDirection(pos, moves, new Vector3(-1, -1, 0), 8);
+                
+                AddAllMovesInDirection(pos, moves, new Vector3(-1, 0, 0), 8);
+                AddAllMovesInDirection(pos, moves, new Vector3(1, 0, 0), 8);
+                AddAllMovesInDirection(pos, moves, new Vector3(0, 1, 0), 8);
+                AddAllMovesInDirection(pos, moves, new Vector3(0, -1, 0), 8);
                 break;
 
             case chessPieces.king:
@@ -104,7 +142,53 @@ public class ChessPieces : MonoBehaviour
                 Gizmos.DrawLine(pos, pos + (transform.up - transform.right).normalized);
                 Gizmos.DrawLine(pos, pos + (-transform.up + transform.right).normalized);
                 Gizmos.DrawLine(pos, pos + (-transform.up - transform.right).normalized);
+                
+                AddAllMovesInDirection(pos, moves, new Vector3(1, 1, 0), 1);
+                AddAllMovesInDirection(pos, moves, new Vector3(-1, 1, 0), 1);
+                AddAllMovesInDirection(pos, moves, new Vector3(1, -1, 0), 1);
+                AddAllMovesInDirection(pos, moves, new Vector3(-1, -1, 0), 1);
+                
+                AddAllMovesInDirection(pos, moves, new Vector3(-1, 0, 0), 1);
+                AddAllMovesInDirection(pos, moves, new Vector3(1, 0, 0), 1);
+                AddAllMovesInDirection(pos, moves, new Vector3(0, 1, 0), 1);
+                AddAllMovesInDirection(pos, moves, new Vector3(0, -1, 0), 1);
+
                 break;
         }
+        
+        Gizmos.color = Color.green;
+        foreach (var move in moves)
+        {
+            Gizmos.DrawSphere(move, 0.1f);
+        }
+    }
+    
+    void AddAllMovesInDirection(Vector3 startPos, List<Vector3> moves,Vector3 direction, int maxSteps)
+    {
+        Vector3 currentPos = startPos;
+
+        for (int step = 1; step <= maxSteps; step++)
+        {
+            currentPos += direction;
+
+            if (currentPos.x < 0 || currentPos.x > 7 || currentPos.y < 0 || currentPos.y > 7)
+                break;
+
+            moves.Add(currentPos);
+        }
+    }
+    
+    
+    Vector3 SnapToGrid(Vector3 position)
+    {
+        int x = Mathf.RoundToInt(position.x);
+        int y = Mathf.RoundToInt(position.y);
+        int z = Mathf.RoundToInt(position.z);
+        
+        x = Mathf.Clamp(x, 0, 7);
+        y = Mathf.Clamp(y, 0, 7);
+        z = 0; // Keep z at 0 for a 2D chessboard
+        
+        return new Vector3(x, y, z);
     }
 }
